8f847524e40b8cc2af624eccf44b98cb727e7632
Cameron Barker <barker.cameron@gmail.com>
2013-06-07 12:18:02 -0400

    adding changes and styling

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -1,3 +1,24 @@
 ActiveAdmin.register Project do
-  form :partial => "form"
+	form :partial => "form"
+
+	config.sort_order = 'position_asc'
+
+	index do
+		column :image do |artwork|
+		   image_tag(artwork.poster(:thumb), :"data-id" => artwork.id, :class => "homeslide")
+		end
+		column :title do |artwork|
+		   content_tag(:span, artwork.title, :"data-id" => artwork.id, :class => "homeslide")
+		end
+		default_actions
+   end
+
+	collection_action :sort, :method => :post do
+		params[:ids].each_with_index do |id, index|
+			artwork = Project.find(id)
+			artwork.update_attribute(:position, index.to_i+1)
+		end
+		head 200
+	end
+
 end
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,2 +1,45 @@
 //= require active_admin/base
 //= require cocoon
+
+
+$(function(){
+
+  $(".clear_filters_btn").click(function(){
+    window.location.search = "";
+    return false;
+  });
+
+  sortingForAdmin("projects");
+});
+
+
+function serializeArtworks(){
+  var artworkIds = $.makeArray(
+    $("#projects .homeslide").map(function(){
+      return $(this).data("id");
+			
+    })
+  );
+	console.log(artworkIds);
+  return {ids: artworkIds};
+};
+
+
+function sortingForAdmin(name){
+ 	$("#projects tbody").sortable({
+				update: function() {
+					$.ajax({
+								  url: name + "/sort",
+						      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+									type: 'post',
+						      data: serializeArtworks(),
+						      complete: function(){
+						        $(".paginated_collection").effect("highlight");
+										//console.log(artworkIds);
+						      }
+								 });
+					}
+		});
+	$("#projects tbody").disableSelection();
+};
+
