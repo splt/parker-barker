207e4bf849e3b5f76562109c37212824dc52f057
Cameron Barker <hellosplt.co@gmail.com>
2013-06-11 01:18:32 -0400

    removing linking functionality from sidebar nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def tag_creator(tag)
-    raw tag.collect { |post| link_to post.name, post.name.downcase.tr(" ", "_") }.join(", ")
+    raw tag.collect { |post| post.name }.join(", ")
   end
 
   def url_slice(url_object)
